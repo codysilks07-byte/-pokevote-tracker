9c074902d8326844ff940318ba7b62a2d2ef2cf1
codysilks07-byte <codysilks07@gmail.com>
2026-07-11 23:01:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from supabase import create_client
 from googleapiclient.discovery import build
 from rapidfuzz import process
 from collections import Counter
@@ -11,7 +12,10 @@ st.title("🎮 PokéVote Tracker")
 
 api_key = st.secrets["YOUTUBE_API_KEY"]
 youtube = build("youtube", developerKey=api_key, version="v3")
-
+supabase = create_client(
+    st.secrets["SUPABASE_URL"],
+    st.secrets["SUPABASE_KEY"]
+)
 pokemon_df = pd.read_csv("pokemon.csv")
 aliases_df = pd.read_csv("aliases.csv")
 drawn_df = pd.read_csv("drawn.csv")
